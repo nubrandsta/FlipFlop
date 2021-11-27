298fa6628cf41521ff19cea044d93f2b6b98a1e1
nubrandsta <ragasukma23@gmail.com>
2021-11-27 22:30:08 +0700

    add register function

--- login.php
+++ login.php
@@ -6,32 +6,115 @@ if(empty($_GET['action'])){
 else{
     $action = $_GET['action'];
 }
-$error = "";
+$forms_1 = '';
+$forms_2 = '';
+$error = 'Selamat Datang';
+
 $conn = mysqli_connect("localhost","root","","db_flop");
-IF($_SERVER["REQUEST_METHOD"]=="POST"){
-    
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    $query_check = mysqli_query($conn, "SELECT EXISTS(SELECT * FROM tb_user WHERE username = '$username') AS 'exist'");
-    $data_check = mysqli_fetch_assoc($query_check);
-    if($data_check["exist"] == 0){
-        $error = "Username ".$username." tidak ada!";
+if($action == "login"){
+    $forms_1 = '<div class="card " id="main-content" style="width:50%">
+        <h3 class="card-title" id="main-content-title">LOG IN</h3>
+            <div class="card-body" id="main-content-body">
+                <p class="card-text" id="main-content-text">';$forms_2='</p>
+                <form action="" method="POST">
+                    <div class="form-group" id="login-form">
+                    <label for="login-username" id="label" >Username</label>
+                    <input type="text" name = "username" class="form-control" id="login-username" placeholder="username">
+                     <label for="login-password" id="label">Password</label>
+                    <input type="text" name = "password" class="form-control" id="login-password" placeholder="password" >
+                    </div>
+                <button type="submit" class="btn btn-primary" id="btn-submit">Login</button>
+                <a class="btn btn-primary" id="btn-register" href="login.php?action=register">Daftar</a>
+                </form>
+            </div>
+            </div>
+        ';
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
         
-    }
-    else{
-        $query_check =mysqli_query($conn, "SELECT password FROM tb_user WHERE username = '$username'");
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $query_check = mysqli_query($conn, "SELECT EXISTS(SELECT * FROM tb_user WHERE username = '$username') AS 'exist'");
         $data_check = mysqli_fetch_assoc($query_check);
-        if($data_check["password"] != $password){
-            $error = "Password salah!";
+        if($data_check["exist"] == 0){
+            $error = "Username ".$username." tidak ada!";
+            
+            
         }
         else{
-            $error = "Password benar";
-            $_SESSION["username"] = $username;
-            header("location: index.php");
+            $query_check =mysqli_query($conn, "SELECT password FROM tb_user WHERE username = '$username'");
+            $data_check = mysqli_fetch_assoc($query_check);
+            if($data_check["password"] != $password){
+                $error = "Password salah!";
+            }
+            else{
+                $error = "Password benar";
+                $_SESSION["username"] = $username;
+                header("location: index.php");
+            }
+    
         }
-
     }
 }
+elseif($action == "register"){
+    $forms_1 = '<div class="card " id="main-content" style="width:50%">
+    <h3 class="card-title" id="main-content-title">REGISTER</h3>
+        <div class="card-body" id="main-content-body">
+            <p class="card-text" id="main-content-text">';
+            $forms_2='</p>
+            <form action="" method="POST">
+                <div class="form-group" id="login-form">
+                <label for="login-username" id="label" >Username</label>
+                <input type="text" name = "username" class="form-control" id="login-username" placeholder="username">
+                 <label for="login-password" id="label">Password</label>
+                <input type="text" name = "password" class="form-control" id="login-password" placeholder="password" >
+                <label for="login-data" id="label">Alamat E-mail</label>
+                <input type="text" name = "email" class="form-control" id="login-data" placeholder="Email" >
+                <label for="login-data" id="label">Nama</label>
+                <input type="text" name = "nama" class="form-control" id="login-data" placeholder="Nama" >
+                </div>
+            <button type="submit" class="btn btn-primary" id="btn-submit">DAFTAR</button>
+            <a class="btn btn-primary" id="btn-back" href="login.php?action=login">Kembali</a>
+            </form>
+        </div>
+        </div>
+    ';
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $name = $_POST["nama"];
+        $email = $_POST["email"];
+        if(empty($username)||empty($password)||empty($name)||empty($email)){
+            $error = "Kolom tidak boleh kosong!";
+        }
+        else{
+            if(!ctype_alnum($username)){
+                $error = "Username hanya dapat mengandung alfanumerik!";
+            }
+            else{
+                $query_check = mysqli_query($conn, "SELECT EXISTS(SELECT * FROM tb_user WHERE username = '$username') AS 'exist'");
+                $data_check = mysqli_fetch_assoc($query_check);
+                 if($data_check["exist"] == 1){
+                     $error = "Username ".$username." sudah diambil!";
+                    }
+                    else{
+                        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                            $error = "Email tidak valid";
+                        }
+                        else{
+                            $register_query = mysqli_query($conn, "INSERT INTO tb_user (username,password,user_level,full_name,email) VALUES ('$username', '$password','2', '$nama', '$email');");
+                            $error = "Mendaftarkan...";
+                            sleep(3);
+                            header("location:login.php?action=login");
+                        }
+                    }
+            }
+        }
+    }
+}
+else{
+    header("location: index.php");
+}
+
 
 
 ?>
@@ -50,14 +133,18 @@ IF($_SERVER["REQUEST_METHOD"]=="POST"){
 
     <style>
         #main-content-title{
-            color:black !important;
+            color:#212529 !important;
             font-weight: bold;
         }
         #login-username{
-            width:90%;
-            margin:auto;
+            width:75%;
+            margin-left:5%;
         }
         #login-password{
+            width:75%;
+            margin-left:5%;
+        }
+        #login-data{
             width:90%;
             margin:auto;
         }
@@ -71,6 +158,20 @@ IF($_SERVER["REQUEST_METHOD"]=="POST"){
             margin-top:3%;
             margin-bottom:3%;
             margin-left:5%;
+        }
+        #btn-register{
+            margin-top:3%;
+            margin-bottom:3%;
+            margin-right:5%;
+            float:right;
+        }
+        #btn-back{
+            margin-top:3%;
+            margin-bottom:3%;
+            margin-right:5%;
+            float:right;
+            background-color:tomato;
+            border-color:tomato;
         }
     </style>
 </head>
@@ -90,21 +191,18 @@ IF($_SERVER["REQUEST_METHOD"]=="POST"){
 </nav>
 <br><br>
 
-<div class="card " id="main-content" style="width:50%">
-<h3 class="card-title" id="main-content-title">LOGIN</h3>
-    <div class="card-body" id="main-content-body">
-        <p class="card-text" id="main-content-text"><?php echo $error;?></p>
-        <form action="" method="POST">
-            <div class="form-group" id="login-form">
-            <label for="login-username" id="label" >Username</label>
-            <input type="text" name = "username" class="form-control" id="login-username" placeholder="username">
-             <label for="login-password" id="label">Password</label>
-            <input type="text" name = "password" class="form-control" id="login-password" placeholder="password" >
-            </div>
-        <button type="submit" class="btn btn-primary" id="btn-submit">Login</button>
-        </form>
-    </div>
-    </div>
+<?php 
+echo $forms_1;
+echo $error;
+echo $forms_2;
+
+if($action == "login"){
+
+}
+elseif($action == "register"){
+    
+}
+?>
 
   
 
